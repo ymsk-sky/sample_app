d9ca86842a1f16a70b49b529f85ccaa59bb859e1
ymsk-sky <ymsk.sky.95@gmail.com>
2018-10-18 01:19:10 +0900

    fix columus -> update_attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
+    update_attributes(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
