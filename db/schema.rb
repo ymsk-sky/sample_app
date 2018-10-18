973dab11c5d6a98cc77cf992a05db16242e22add
ymsk-sky <ymsk.sky.95@gmail.com>
2018-10-18 22:38:59 +0900

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20181018045324) do
     t.boolean "admin", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
-    t.datetime "activated_at"
+    t.time "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
