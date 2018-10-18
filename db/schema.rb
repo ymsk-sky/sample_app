22b5e2512a37c1b188891c470b22086a0bbb90b5
ymsk.sky <ymsk.sky.95@gmail.com>
2018-10-18 14:02:28 +0900

    fin 12.1.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015105547) do
+ActiveRecord::Schema.define(version: 20181018045324) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20181015105547) do
     t.boolean "admin", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
-    t.time "activated_at"
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
